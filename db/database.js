b5c6fe5d29edfd1b84e34e3117e4102a3646960c
Amu Timilehin <amuoladipupo@gmail.com>
2023-09-10 19:35:38 +0100

    xawftly debugging
    Database and server error debugging phase 2

--- db/database.js
+++ db/database.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 let connection;
 try{
     mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+    mongoose.set("strictQuery", true);
     connection = true;
 }catch(error){
     connection = error;
